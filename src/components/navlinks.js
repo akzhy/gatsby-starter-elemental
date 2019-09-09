02e413130c8662edb7bcab66f108b3c0df792a17
Akshay <akshay48884@gmail.com>
2019-09-09 21:43:21 +0530

    Fix issue with theme

--- src/components/navlinks.js
+++ src/components/navlinks.js
@@ -42,48 +42,53 @@ class ThemeSwitchButton extends React.Component {
         };
     }
 
-    componentDidMount() {
+    componentWillMount(){
         const _this = this;
-
         let darkMode = localStorage.getItem("darkMode");
-        document.body.className = this.state.darkMode ? "dark-mode" : "";
 
         if (darkMode) {
             this.setState({
                 darkMode: (darkMode === "true")
             }, () => {
-                document.body.className = this.state.darkMode ? "dark-mode" : "";
+                document.body.className = _this.state.darkMode ? "dark-mode" : "";
             })
         }
+    }
+
+    componentDidMount() {
+        const _this = this;
 
         this.switchBtn.addEventListener("click", function() {
             _this.setState({
                 darkMode: !_this.state.darkMode
             });
             localStorage.setItem("darkMode", _this.state.darkMode);
+            
             document.body.className = _this.state.darkMode ? "dark-mode" : "";
         });
     }
     render() {
         return (
-            <li className="switch-theme">
-                <div className="switch-button" ref={c => (this.switchBtn = c)}>
-                    <div
-                        title="Switch to Dark Mode"
-                        data-switch-to="dark"
-                        className={!this.state.darkMode ? "active" : ""}
-                    >
-                        <Sun />
+            <React.Fragment>
+                <li className="switch-theme">
+                    <div className="switch-button" ref={c => (this.switchBtn = c)}>
+                        <div
+                            title="Switch to Dark Mode"
+                            data-switch-to="dark"
+                            className={!this.state.darkMode ? "active" : ""}
+                        >
+                            <Sun />
+                        </div>
+                        <div
+                            title="Switch to Light Mode"
+                            data-switch-to="light"
+                            className={this.state.darkMode ? "active" : ""}
+                        >
+                            <Moon />
+                        </div>
                     </div>
-                    <div
-                        title="Switch to Light Mode"
-                        data-switch-to="light"
-                        className={this.state.darkMode ? "active" : ""}
-                    >
-                        <Moon />
-                    </div>
-                </div>
-            </li>
+                </li>
+            </React.Fragment>
         );
     }
 }
