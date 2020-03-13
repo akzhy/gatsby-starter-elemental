a585223c66d53af0a22154fc8657c5245d04310c
akzhy <akshay48884@gmail.com>
2020-03-13 22:07:51 +0530

    Optionally show disqus

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React from "react";
 
 import { StaticQuery, graphql } from "gatsby";
 
-import { Disqus } from 'gatsby-plugin-disqus'
+import { Disqus } from "gatsby-plugin-disqus";
 
 function Comments({ data, title, location }) {
-  const disqusConfig = {
-    url: `${data.site.siteMetadata.siteUrl + location}`,
-    identifier: title,
-    title: title,
-  }
-  return (
-    <Disqus config={disqusConfig} />
-  )
+    if (data.site.siteMetadata.disqus === null) return <></>;
+
+    const disqusConfig = {
+        url: `${data.site.siteMetadata.siteUrl + location}`,
+        identifier: title,
+        title: title
+    };
+    return <Disqus config={disqusConfig} />;
 }
 
 export default ({ title, location }) => {
-  return (
-    <StaticQuery
-      query={graphql`
-            query {
-                site {
-                    siteMetadata {
-                        siteUrl
+    return (
+        <StaticQuery
+            query={graphql`
+                query {
+                    site {
+                        siteMetadata {
+                            siteUrl
+                            disqus
+                        }
                     }
                 }
-            }
-        `}
-      render={data => <Comments data={data} title={title} location={location} />}
-    />
-  )
-}
+            `}
+            render={data => (
+                <Comments data={data} title={title} location={location} />
+            )}
+        />
+    );
+};
