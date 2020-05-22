f8fd4fab5148277c506cdc0cb2dab49e2e3f72fc
akzhy <akshay48884@gmail.com>
2020-05-22 20:59:55 +0530

    Portfolio page ⚡

--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -1,21 +1,54 @@
-import React from "react";
-import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import Img from "gatsby-image";
-import SEO from "../components/seo";
+import React from "react"
+import { MDXProvider } from "@mdx-js/react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Img from "gatsby-image"
+import { Calendar } from "react-feather"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 
-export default function({ location }) {
+import { Row, Col } from "../components/shortcodes/index"
+
+export default function({ location, data }) {
+    console.log(data)
     return (
-        <Layout seo={{
-            title: "Portfolio page"
-        }}
-        location={location}>
-                        {/*
-            <SEO
-                lang="en"
-                title={data.markdownRemark.frontmatter.title}
-                description={data.markdownRemark.frontmatter.description}
-            />
+        <Layout
+            seo={{
+                title: "Portfolio page",
+            }}
+            location={location}
+        >
+            <div className="md:px-4 mt-12 py-6 md:w-11/12 mx-auto">
+                <div className="mx-auto relative">
+                    <Img
+                        fluid={
+                            data.mdx.frontmatter.banner.childImageSharp.fluid
+                        }
+                    />
+                    <div className="flex items-center justify-center absolute w-full h-full top-0 left-0">
+                        <div className="absolute w-full h-full bg-black opacity-50"></div>
+                        <div className="relative z-10 text-center">
+                            <h1 className="text-5xl text-white font-bold">
+                                {data.mdx.frontmatter.title}
+                            </h1>
+                            <p className="mt-1 text-white flex items-center justify-center">
+                                <Calendar />{" "}
+                                <span className="ml-2">
+                                    {data.mdx.frontmatter.date}
+                                </span>
+                            </p>
+                            <p className="mt-3 text-white md:w-3/4 mx-auto">
+                                {data.mdx.frontmatter.description}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="lg:w-3/4 md:w-11/12 sm:w-full p-3 md:mt-6 mx-auto lg:mt-12">
+                    <MDXProvider components={{ Row, Col }}>
+                        <MDXRenderer>{data.mdx.body}</MDXRenderer>
+                    </MDXProvider>
+                </div>
+            </div>
+            {/*
 
             <div className="container">
                 <article className="post">
@@ -46,18 +79,18 @@ export default function({ location }) {
             </div>
                         {*/}
         </Layout>
-    );
+    )
 }
 
-/*
 export const query = graphql`
     query($slug: String!) {
         mdx(fields: { slug: { eq: $slug } }) {
-            html
+            body
             frontmatter {
                 title
+                date(formatString: "DD MMMM YYYY")
                 description
-                image {
+                banner {
                     publicURL
                     childImageSharp {
                         fluid(maxWidth: 1920) {
@@ -70,5 +103,4 @@ export const query = graphql`
             }
         }
     }
-`;
-*/
+`
