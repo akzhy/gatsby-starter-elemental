2175cee7cd9275527bb630b8cb895cd0579b024c
akzhy <akshay48884@gmail.com>
2020-05-31 21:55:26 +0530

    Optional contact form ⚡

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,9 @@ import Layout from "../components/layout"
 import { Form, Description } from "../components/contact"
 
 export default ({data, location}) => {
+
+    const api_url = data.site.siteMetadata.contact.api_url;
+    const hasContactForm = (api_url);
     return (
         <Layout
             seo={{
@@ -19,10 +22,12 @@ export default ({data, location}) => {
                     </h2>
                 </div>
                 <div className="flex flex-wrap pb-40">
-                    <div className="w-full lg:w-1/2 px-6">
-                        <Form api={data.site.siteMetadata.contact.api_url}/>
+                    {hasContactForm &&
+                        <div className="w-full lg:w-1/2 px-6">
+                        <Form api={api_url}/>
                     </div>
-                    <div className="w-full lg:w-1/2 px-6 pt-8">
+                    }
+                    <div className={`w-full ${hasContactForm ? 'lg:w-1/2' : 'lg:w-2/3 mx-auto'} px-6 pt-8`}>
                         <Description data={data.site.siteMetadata.contact} />
                     </div>
                 </div>
