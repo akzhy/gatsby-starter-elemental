2a5ca1200ea2a3f372f1e9461a2e63223f8ffb94
akzhy <akshay48884@gmail.com>
2020-05-31 21:51:53 +0530

    Clean up 🔥

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,36 +21,6 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     }
 }
 
-exports.createSchemaCustomization = ({ actions }) => {
-    const { createTypes } = actions
-    const typeDefs = `
-      type Site implements Node {
-        siteMetadata: SiteMetaData
-      }
-      type SiteMetaData {
-        disqus: String
-      }
-    `
-    createTypes(typeDefs)
-
-    /*const { createTypes } = actions;
-    const typeDefs = [
-        `type Site implements Node { SiteMetadata: SiteMetaData }`,
-        schema.buildObjectType({
-            name: "SiteMetadata",
-            fields: {
-                name: "String!",
-                firstName: "String!",
-                email: "String!",
-                receivedSwag: {
-                    type: "Boolean",
-                    resolve: source => source.receivedSwag || false
-                }
-            },
-            interfaces: ["Node"]
-        })
-    ];*/
-}
 
 exports.createPages = ({ graphql, actions }) => {
     const { createPage } = actions
@@ -136,88 +106,5 @@ exports.createPages = ({ graphql, actions }) => {
                 },
             })
         })
-
-        // console.log(result);
-        //console.log(result);
-        /*const blogPosts = result.data.blog.edges;
-
-        const blogPostsPerPage =
-            result.data.limitPost.siteMetadata.blogItemsPerPage;
-        const numBlogPages = Math.ceil(blogPosts.length / blogPostsPerPage);
-
-        Array.from({ length: numBlogPages }).forEach((_, i) => {
-            createPage({
-                path: i === 0 ? `/blog` : `/blog/${i + 1}`,
-                component: path.resolve("./src/templates/blog-list.js"),
-                context: {
-                    limit: blogPostsPerPage,
-                    skip: i * blogPostsPerPage,
-                    numPages: numBlogPages,
-                    currentPage: i + 1
-                }
-            });
-        });
-
-        const PortfolioItems = result.data.portfolio.edges;
-        const PortfolioItemsPerPage =
-            result.data.limitPost.siteMetadata.portfolioItemsPerPage;
-        const numPortfolioItems = Math.ceil(
-            PortfolioItems.length / PortfolioItemsPerPage
-        );
-
-        Array.from({ length: numPortfolioItems }).forEach((_, i) => {
-            createPage({
-                path: i === 0 ? `/portfolio` : `/portfolio/${i + 1}`,
-                component: path.resolve("./src/templates/portfolio-list.js"),
-                context: {
-                    limit: blogPostsPerPage,
-                    skip: i * blogPostsPerPage,
-                    numPages: numPortfolioItems,
-                    currentPage: i + 1
-                }
-            });
-        });
-
-        result.data.blog.edges.forEach(({ node }) => {
-            let template =
-                node.frontmatter.template === undefined
-                    ? "blog"
-                    : node.frontmatter.template;
-            createPage({
-                path: node.fields.slug,
-                component: path.resolve("./src/templates/" + template + ".js"),
-                context: {
-                    slug: node.fields.slug
-                }
-            });
-        });
-
-        result.data.portfolio.edges.forEach(({ node }) => {
-            let template =
-                node.frontmatter.template === undefined
-                    ? "portfolio"
-                    : node.frontmatter.template;
-            createPage({
-                path: node.fields.slug,
-                component: path.resolve("./src/templates/" + template + ".js"),
-                context: {
-                    slug: node.fields.slug
-                }
-            });
-        });
-
-        result.data.basepages.edges.forEach(({ node }) => {
-            let template =
-                node.frontmatter.template === undefined
-                    ? "basepage"
-                    : node.frontmatter.template;
-            createPage({
-                path: node.fields.slug,
-                component: path.resolve("./src/templates/" + template + ".js"),
-                context: {
-                    slug: node.fields.slug
-                }
-            });
-        });*/
     })
 }
